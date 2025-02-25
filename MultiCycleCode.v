b4fb457fdcb83cde0577f2bfa26ad9116f7ba0ec
Charlieborge <cborge2004@gmail.com>
2025-02-24 20:14:00 -0500

    Update MultiCycleCode.v

--- MultiCycleCode.v
+++ MultiCycleCode.v
@@ -245,20 +245,20 @@ always @(posedge clk) begin
 	    
    // CARLOS B
    // RAND8 Instruction: rand8 $rd, $rs  --> rd = (13*rs)%256
-      100: begin `CONST(3) `Yin `NEXT end        // Load constant 3 into Y for shift left by 3
-      101: begin `SELrs `REGout `ALUsll `ALUZin `NEXT end // Compute (rs << 3); ALUZ = rs << 3
-      102: begin `ALUZout `SELrd `REGin `NEXT end       // Save term1 (rs << 3) into destination ($rd)
-      103: begin `CONST(2) `Yin `NEXT end        // Load constant 2 into Y for shift left by 2
-      104: begin `SELrs `REGout `ALUsll `ALUZin `NEXT end  // Compute (rs << 2); ALUZ = rs << 2
-      105: begin `SELrd `REGout `Yin `NEXT end       // Load current rd (term1) into Y
-      106: begin `ALUZout `ALUadd `ALUZin `NEXT end    // Add: (rs << 3) + (rs << 2); ALUZ = term1 + term2
-      107: begin `ALUZout `SELrd `REGin `NEXT end        // Save sum (12*rs) into rd     
-      108: begin `SELrd `REGout `Yin `NEXT end        // Load current rd (12*rs) into Y
-      109: begin `SELrs `REGout `ALUadd `ALUZin `NEXT end  // Add rs: 12*rs + rs = 13*rs; ALUZ = 13*rs
-      110: begin `ALUZout `SELrd `REGin `NEXT end         // Save result (13*rs) into rd
-      111: begin `CONST(8'hFF) `Yin `NEXT end        // Load mask 0xFF into Y
-      112: begin `SELrd `REGout `ALUand `ALUZin `NEXT end // Compute (13*rs) & 0xFF; ALUZ = final 8-bit result
-      113: begin `ALUZout `SELrd `REGin `JUMP(0) end     // Write final result into rd and return to fetch
+      50: begin `CONST(3) `Yin `NEXT end        // Load constant 3 into Y for shift left by 3
+      51: begin `SELrs `REGout `ALUsll `ALUZin `NEXT end // Compute (rs << 3); ALUZ = rs << 3
+      52: begin `ALUZout `SELrd `REGin `NEXT end       // Save term1 (rs << 3) into destination ($rd)
+      53: begin `CONST(2) `Yin `NEXT end        // Load constant 2 into Y for shift left by 2
+      54: begin `SELrs `REGout `ALUsll `ALUZin `NEXT end  // Compute (rs << 2); ALUZ = rs << 2
+      55: begin `SELrd `REGout `Yin `NEXT end       // Load current rd (term1) into Y
+      56: begin `ALUZout `ALUadd `ALUZin `NEXT end    // Add: (rs << 3) + (rs << 2); ALUZ = term1 + term2
+      57: begin `ALUZout `SELrd `REGin `NEXT end        // Save sum (12*rs) into rd     
+      58: begin `SELrd `REGout `Yin `NEXT end        // Load current rd (12*rs) into Y
+      59: begin `SELrs `REGout `ALUadd `ALUZin `NEXT end  // Add rs: 12*rs + rs = 13*rs; ALUZ = 13*rs
+      60: begin `ALUZout `SELrd `REGin `NEXT end         // Save result (13*rs) into rd
+      61: begin `CONST(8'hFF) `Yin `NEXT end        // Load mask 0xFF into Y
+      62: begin `SELrd `REGout `ALUand `ALUZin `NEXT end // Compute (13*rs) & 0xFF; ALUZ = final 8-bit result
+      63: begin `ALUZout `SELrd `REGin `JUMP(0) end     // Write final result into rd and return to fetch
 
 
   
